31ccbe672413280422153536410741740760550e
Jeff <jr0097@uah.edu>
2018-11-06 21:05:09 -0600

    this one works as expected

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@
 
 import requests
 url = 'https://api.groupme.com/v3/bots/post'
-headers = {'Content-type': 'application/json'}
-data = {'text': 'RISE AND GRIND YOU CRAZY KIDS'}
-params = {'bot_id': '2e542107791110329aabd6cbd9-8'}
-r = requests.post(url=url, data=data, headers=headers, params=params)
+m_Text = "LETS GO"
+data = '{"text" : "RISE AND GRIND YOU CRAZY KIDS ", "bot_id" : "2e542107791110329aabd6cbd9"}'
+response = requests.post(url, data=data)
