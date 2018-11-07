ecbe655961de5d5b2c88d1af7bf64c68c829365f
Jeff <jr0097@uah.edu>
2018-11-06 21:40:55 -0600

    added secret function to chose random item to throw into text field

--- bot.py
+++ bot.py
@@ -2,9 +2,28 @@
 # initial group me Bot file
 
 import requests
+import secrets
+# initial groupme API URL
 url = 'https://api.groupme.com/v3/bots/post'
-m_Text = "LETS GO"
+
+# nouns
+m_Nouns = ['YOU CRAZY KIDS',
+        'YOU SHITBIRDS',
+        'ANDREW "RICKY C" MUNSTER']
+
+# shouts
+m_Shouts = ['LETS GO!',
+            'AND EAT SHIT CHANG',
+            'TECH HALL STILL SMELLS BAD',
+            'IM SORRY ABOUT MY LATE FEE RIA',
+            'EVEN U ZHANG',
+            'LETS GET THIS BREAD']
+
+
+# build the request 
 data = {
-        "text" : "RISE AND GRIND YOU CRAZY KIDS {0}".format(m_Text), 
+        "text" : "RISE AND GRIND {0}, {1}".format(secrets.choice(m_Nouns), secrets.choice(m_Shouts)), 
         "bot_id" : "2e542107791110329aabd6cbd9"}
+
+# Run that shit
 response = requests.post(url, data=data)
